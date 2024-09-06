31d18a39d125804e0983c738644afa5db439d0fb
PSeasword <philip.sjosward@gmail.com>
2024-09-06 04:15:03 -0700

    Comments in SCRIPT_TEMPLATE.py now references metamodel/configuration and system/setup views

--- scripts/SCRIPT_TEMPLATE.py
+++ scripts/SCRIPT_TEMPLATE.py
@@ -1,10 +1,10 @@
 # Available functions:
 
-# Meaning of values passed to methods for finding/matching blocks in setup views, where None considers all:
+# Meaning of values passed to methods for finding/matching blocks, where None considers all:
 # class_type: Name of class type (for example, Attack event)
 # class_instance: Name of class instance (for example, DoS attack)
 # attribute: Name of attribute (for example, Local difficulty)
-# view: Setup view name to consider, where None considers all
+# view: System/setup view name to consider, where None considers all
 
 # Values passed to the methods should either be a string or a float/integer
 
@@ -12,21 +12,21 @@
 #     Returns the name of the current view
 
 # script_if.get_class_type_names(view=None)
-#     Returns a list of class names (those specified in configuration views) found in the specified setup views
+#     Returns a list of class names (those specified in metamodel/configuration views) found in the specified system/setup views
 
 # script_if.get_class_instance_names(class_type, view=None)
-#     Returns a list of class instance names (those specified in setup views) found in the specified setup views
+#     Returns a list of class instance names (those specified in system/setup views) found in the specified system/setup views
 
 # script_if.get_attribute_names(class_type)
 #     Returns a list of attribute names for a specific class type
 
 # script_if.get_input_class_names(class_type, class_instance, *, input_class_type=None, input_class_instance=None, view=None)
-#     Returns a list of tuples including the class type and class instance names of all classes which the specified setup class instance takes input from, considering the optional filtering of classes
-#     [(input_class_type, input_class_instance), ...]
+#     Returns a list of tuples including the class type and class instance names of all classes which the specified system/setup class instance takes input from, considering the optional filtering of classes
+#     Example: [("Attack event AND", "DDoS attack"), ...]
 
 # script_if.get_attribute_values(class_type, class_instance, attribute, view=None)
-#     Returns a list of the values displayed by the specified setup attributes, each displayed value being represented by a list of each individual values
-#     [(1, 2, 3), (0.45,), ("Text",), ...]
+#     Returns a list of the values displayed by the specified attributes, each displayed value being represented by a tuple
+#     Example: [(1, 2, 3), (0.45,), ("Text",), ...]
 
 # script_if.convert_value_to_string(attribute_value)
 #     Returns the specified attribute tuple value as a formatted string
@@ -41,7 +41,7 @@
 #     Adds a visual marker on all matching class instances
 
 # script_if.calculate_values()
-#     Calculates all attribute values in the setup views based on the current configuration
+#     Calculates all attribute values
 
 def script_logic(script_if):
     # Insert logic here
