408e97f0d3824f59e2d01ddd67e70dae85b89ea4
Lukas <gamard@kth.se>
2025-05-19 11:05:26 +0200

    Edit setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="yacraf_calculator",
     version="1.0.1",
     description="A calculator for a YACRAF instance",
-    packages=find_packages("yacraf_calculator"),  # Automatically find all Python packages inside `src`
+    packages=find_packages(where="."),  # Automatically find all Python packages inside `src`
     install_requires=["numpy"],  # Dependencies
     long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown"
